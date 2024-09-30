a439a0e270f3eb7269c211e0c6530ac59911f42e
Luke <luke.michalek@gmail.com>
2024-09-30 22:40:50 +0100

    ADD Event Listener for Button and dynamic content
    - Add onSelectUser listener to Button
    - Randomly generated content(user is randomly chosen from the list of random users - DUMMY_USERS ) as an object containing id, name and link to avatar nad this is then being displayed. Note that we use random index so there will be always different user after refresh.

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -4,6 +4,8 @@ import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
+// randomIndex generates random number from 0 - DUMMY_USERS.length
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -15,7 +17,21 @@ export class UserComponent {
 
   selectedUser = DUMMY_USERS[randomIndex]
 
+  // Getter to get cleaner use of property binding
+  get imagePath () {
+
+    return 'assets/users/' + this.selectedUser.avatar
+
+  }
+
+  // Event Listener for button
+  onSelectUser () {
+    console.log('Clicked')
+  }
+
 }
+
+
 
 
 
