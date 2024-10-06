5cb06fd640be211255e5b4a033bfec6c4998e3ba
Luke <luke.michalek@gmail.com>
2024-10-06 20:35:00 +0100

    ADD Signal functionality to Button
    - Replace standing functionality of Angular Change Detection Mechanism to Signal-based Mechanism

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+// * To create a signal first I need to import it from angular core
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
+import { computeMsgId } from '@angular/compiler';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
@@ -13,40 +15,99 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
-  selectedUser = DUMMY_USERS[randomIndex]
+// Using Angulars State Change Management
+// export class UserComponent {
 
-  // Getter to get cleaner use of property binding
-  get imagePath () {
+//   // * This uses zone.js part of Angular where states changes automatically
+//    selectedUser = DUMMY_USERS[randomIndex]
+ 
+//   // Getter to get cleaner use of property binding
+//   get imagePath () {
 
-    return 'assets/users/' + this.selectedUser.avatar
+//     return 'assets/users/' + this.selectedUser.avatar
 
-  }
+//   }
 
-  // Event Listener for button
+//   // Event Listener for button
+//   onSelectUser () {
+
+
+//     // console.log('Clicked')
+
+//   /**
+//    * const randomIndex is a locally scoped constant that is ONLY available inside of onSelectUser and that overrides the global const randomIndex, and the randomIndex is recalculated every time the function onSelectUser(when we click) is run
+//    * 
+//    * ! Note that Since I am referring to that selectedUser property from INSIDE THE CLASS instead of from INSIDE THE TEMPLATE as I did before I need to add 'this' to the property.
+
+//    */
+    
+//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    
+//     // * This is how can state be changed without using signal
+//     // this.selectedUser = DUMMY_USERS[randomIndex]
+
+
+
+
+
+//   }
+
+// }
+
+// export class UserComponent {
+
+//   // * This uses zone.js part of Angular where states changes automatically
+//    selectedUser = DUMMY_USERS[randomIndex]
+ 
+//   // Getter to get cleaner use of property binding
+//   get imagePath () {
+
+//     return 'assets/users/' + this.selectedUser.avatar
+
+//   }
+
+//   // Event Listener for button
+//   onSelectUser () {
+
+
+//     // console.log('Clicked')
+
+//   /**
+//    * const randomIndex is a locally scoped constant that is ONLY available inside of onSelectUser and that overrides the global const randomIndex, and the randomIndex is recalculated every time the function onSelectUser(when we click) is run
+//    * 
+//    * ! Note that Since I am referring to that selectedUser property from INSIDE THE CLASS instead of from INSIDE THE TEMPLATE as I did before I need to add 'this' to the property.
+
+//    */
+    
+//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    
+//     // * This is how can state be changed without using signal
+//     // this.selectedUser = DUMMY_USERS[randomIndex]
+
+
+
+
+
+//   }
+
+// }
+
+// Using Signals
+export class UserComponent{
+
+  selectedUser = signal(DUMMY_USERS[randomIndex])
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
   onSelectUser () {
 
-
-    // console.log('Clicked')
-
-  /**
-   * const randomIndex is a locally scoped constant that is ONLY available inside of onSelectUser and that overrides the global const randomIndex, and the randomIndex is recalculated every time the function onSelectUser(when we click) is run
-   * 
-   * ! Note that Since I am referring to that selectedUser property from INSIDE THE CLASS instead of from INSIDE THE TEMPLATE as I did before I need to add 'this' to the property.
-
-   */
-    
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex]
-
-
-
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
 
   }
 
-}
 
+}
 
 
 
