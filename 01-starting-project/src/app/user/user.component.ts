b57978d3804c4e5baad6fef05f186cbbc074fabb
Luke <luke.michalek@gmail.com>
2024-10-30 20:38:57 +0000

    Use Object Type as an Input
    - This section explores the use of Object Type as an input.

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -14,36 +14,44 @@ import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 export class UserComponent{
 
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
 
-// *****     LESSON CONTINUES - 3     **********//
 
-// By using {required: true} I will get an error during development If I would forget to set some of those inputs and therefore, even though I have said that there won`t be an undefined value by using '!' ===> Type 'undefined' is not assignable to type 'string'.ngtsc(2322)
 
-//H Continue to APP.COMPONENTS.TS
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+
+  // * I can simplify this code so that instead of accpeting 3 properties, I will accpet just one proeprty => The entire user object
+
+  @Input({required: true}) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  }; 
+
+  // Remember! By using 'user!' I am convincing TypeScript that value will be defined! And by adding {required: true} will make sure to show an error in IDE in case I would forgot to add value and therefore there will be undefined at the end. This is a safeguard against errors. 
+
+
 
   @Output() select = new EventEmitter<string>();
 
 
-
+// Of course by using object type I need to adjust the code accordingly
 
   get imagePath(){
-    return 'assets/users/' + this.avatar
+    // return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
 
 
   userClicked() {
 
-    this.select.emit(this.id);
+    // this.select.emit(this.id);
+    this.select.emit(this.user.id);
 
     
   }
 
-// logUserNameInUserComponent(){
-//   this.userName.emit(this.name);
-// }
 }
 
 
