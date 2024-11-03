08e9fc7219996c77b7987c761e0b402f4cd279a3
Luke <luke.michalek@gmail.com>
2024-11-03 13:45:59 +0000

    Output Task Data in the Task Component
    - The Task component now displays actual data from task objects instead of just placeholder data as it did previously.

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+
+interface Task {
+
+  id: string,
+  userId: string,
+  title: string,
+  summary: string,
+  dueDate:string
+
+}
+
 
 @Component({
   selector: 'app-task',
@@ -8,5 +20,12 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
+
+  @Input({required: true}) task!: Task; 
+
+  // Note '!' ===> I know that I will always get value! .... 
+  // {required: true} ===> If it should for some reason happen that I would not get a defined value (EVEN THOUGH I HAVE SAID I ALWAYS WILL ==> '!') Angular will let me know through error
+
+  //* Here I have make use of Interface so I do not have to Input all different properties, but instead I will Input it as an object and then access every property
 
 }
