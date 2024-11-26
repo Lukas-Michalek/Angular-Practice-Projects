ba51fae056738d12d5dce86a926b8b060ee20d24
Luke <luke.michalek@gmail.com>
2024-11-26 07:51:52 +0000

    Calculate the Annual Investment Data (DESTRUCTURING)
    Introduction to handling user input for calculating the annual investment data using the JavaScript concept of DESTRUCTURING.
    
    Explains the correct way to handle variable types as function parameters.

--- Bank-Investment-App/src/app/app.component.ts
+++ Bank-Investment-App/src/app/app.component.ts
@@ -16,15 +16,48 @@ import { type RecordDataType } from './user-input/user-input.model'
 })
 export class AppComponent {
 
+  //H Calculating Investment Data -> In Component Aproach
 
-  annualData(annualDataReceived: any[]){
-
-    console.log('These are the data received ' + annualDataReceived)
-  }
+  //* Note the that each parameter needs to have proeprty type and the function needs to recive its argument in this shape! 
   
-  
+  calculateInvestmentResults(data: {
 
+    initialInvestment: number;
+    duration: number;
+    annualInvestment: number;
+    expectedReturn: number;
+})   
+  {
+
+    //!!! To unpack the data I am using JavaScript DESTRUCTURING
+    
+    const { initialInvestment, duration, annualInvestment, expectedReturn } = data;
+
+    const annualData = [];
+    let investmentValue = initialInvestment;
+  
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      
+        annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+  
+    return annualData;
   }
+
+  
 
 
 
